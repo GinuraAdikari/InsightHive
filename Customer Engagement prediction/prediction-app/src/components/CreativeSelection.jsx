8d636e45f018218a7d58436201ad7181b2c8ad48
Abdullah-Nazly <abdullahibnunazly@gmail.com>
2025-02-26 22:53:17 +0530

    Changing the number of inputs. As a results, modified preprocessing with modified graph

--- Customer Engagement prediction/prediction-app/src/components/CreativeSelection.jsx
+++ Customer Engagement prediction/prediction-app/src/components/CreativeSelection.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 
-const CreativeSelection = ({ formData, handleSelectChange }) => (
-    <div>
-        <label htmlFor="creative">Creative:</label>
-        <select id="creative" onChange={handleSelectChange}>
-            <option value="">Select Creative</option>
-            {["1500", "0"].map((option, index) => (
-                <option key={index} value={index}>
-                    {option}
-                </option>
-            ))}
-        </select>
+const CreativeSelection = ({ formData, handleSelectChange }) => {
+  return (
+    <div className="creative-selection">
+      <div>
+        <label htmlFor="creative">Creative: Yes</label>
+        <input
+          type="radio"
+          id="creative"
+          name="creative"
+          value="0"
+          checked={formData.creative[0] === 1}
+          onChange={handleSelectChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="creative">No</label>
+        <input
+          type="radio"
+          id="creative"
+          name="creative"
+          value="1"
+          checked={formData.creative[1] === 1}
+          onChange={handleSelectChange}
+        />
+      </div>
     </div>
-);
+  );
+};
 
 export default CreativeSelection;
