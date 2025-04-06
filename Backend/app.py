4d82e24f6cf3fb62967ad7f79316998c78549cd9
Abdullah-Nazly <abdullahibnunazly@gmail.com>
2025-04-06 07:46:47 +0530

    Uncommented Danuji's backend area

--- Backend/app.py
+++ Backend/app.py
@@ -2,7 +2,7 @@
 from flask import Flask
 from routes.recommendation_engine_route.rec import rec_bp  # Import the Blueprint from rec.py
 from routes.customer_engagement_route.engagement_pred import engagement_bp
-# from routes.absa_route.absa_bp import absa_bp
+from routes.absa_route.absa_bp import absa_bp
 
 # Create the Flask app
 app = Flask(__name__)
@@ -14,7 +14,7 @@ app.register_blueprint(engagement_bp, url_prefix='/engagement')
 app.register_blueprint(rec_bp, url_prefix='/recommendation')
 
 # Register the ABSA Blueprint with a URL prefix
-# app.register_blueprint(absa_bp, url_prefix='/absa')
+app.register_blueprint(absa_bp, url_prefix='/absa')
 
 print(app.url_map)
 
